e44281719a8b6ea4706a837c61dc48a2d3aa0c9b
unknown <minhsunnybear@gmail.com>
2025-02-07 11:38:25 +0700

    New features

---
+++ api_key.py
@@ -0,0 +1,2 @@
+CLIENT_ID = "1094056921998-a602uc78ptb9pvvj8qcudojap54p7aih.apps.googleusercontent.com"
+CLIENT_SECRET = "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"
--- app.py
+++ app.py
@@ -11,13 +11,14 @@ import jwt
 import cloudinary
 import cloudinary.uploader
 import requests
-from google.oauth2 import id_token
-from google.auth.transport import requests as google_requests
+from authlib.integrations.flask_client import OAuth
+from api_key import *
 
 from helpers import login_required
 
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'
+app.secret_key = os.getenv("SECRET_KEY", "fallback_secret_key")
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
@@ -25,15 +26,28 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Connect to MongoDB
-client = MongoClient("connection_string_here")
-db = client["your_database_name"]
+client = MongoClient("mongodb+srv://minh_huynh:p6JI9yazQsuyknnZ@cluster0.hlktt.mongodb.net/user_data?retryWrites=true&w=majority")
+db = client["user_data"]
 users_collection = db["users"]
+
+# Google Authentication
+oauth = OAuth(app)
+google = oauth.register(
+    name='google',
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    server_metadata_uri='https://accounts.google.com/.well-known/openid-configuration',
+    client_kwargs={'scope' : 'openid profile email'}
+)
 
 # JWT secret key #TODO
 
 # Cloudinary config #TODO
 
 @app.route("/")
+# This is the sign-up/login page
+def index():
+    return render_template("index.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -46,19 +60,19 @@ def login():
 
         if not email or not password:
             flash("Please fill in all fields!")
-            return redirect("/login")
+            return render_template("login.html")
 
         # Get user's data from MongoDB database
         user = users_collection.find_one({"email": email})
 
         if not user or not check_password_hash(user["password_hash"], password):
             flash("Invalid username and/or password")
-            return redirect("/login")
+            return render_template("login.html")
         
         # Remember which user has logged in
         session["user_id"] = str(user["_id"])
 
-        return redirect("/")
+        return redirect("/main")
     else:
         return render_template("login.html")
 
@@ -75,13 +89,13 @@ def signup():
 
         if not username or not email or not password:
             flash("Please fill in all fields!")
-            return redirect("/signup")
+            return render_template("signup.html")
         
         user = users_collection.find_one({"username": username})
 
         if user:
             flash("This username already exists!")
-            return redirect("/signup")
+            return render_template("signup.html")
         
         hash_pass = generate_password_hash(password)
         
@@ -97,11 +111,51 @@ def signup():
         return render_template("signup.html")
 
 
-@app.route("/login/google", methods=["GET", "POST"])
+@app.route("/login/google")
 def google_login():
     """Log_in using Google"""
+    try:
+        redirect_uri = url_for('authorize',_external=True)
+        return google.authorize_redirect(redirect_uri)
+    except Exception as e:
+        flash("Error during login!")
+        return redirect("/login")
 
 
-@app.route("/authorize/google", methods=["GET", "POST"])
+@app.route("/authorize/google")
 def google_auth():
-    """Authorize users"""
+    """Authorize users for Google"""
+    token = google.authorize_access_token()
+    userinfo_endpoint = google.server_metadata['userinfo_endpoint']
+    resp = google.get(userinfo_endpoint)
+    user_info = resp.json()
+    username = user_info['email']
+
+    # Connect to MongoDB
+    user = users_collection.find_one({"email" : username})
+    if not user:
+        new_user = {
+            "username": user_info.get("name", ""),
+            "email": user_info["email"],
+        }
+        users_collection.insert_one(new_user)
+        user = new_user
+        
+    session["username"] = username
+    session["oauth_token"] = token
+
+    return redirect("/main")
+
+
+@app.route("/main")
+# This is the map visualization page
+def main():
+    return render_template("main.html")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
